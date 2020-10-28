dc1010ccffba60006422d308db0fa05ca5b237e2
Kevin Jones <kevin.jones@ppushdr.com>
2020-10-28 12:11:58 +0000

    refactor and reformat with prettier

--- create.ts
+++ create.ts
@@ -1,48 +1,22 @@
-import * as uuid from 'uuid';
-import { AWSError, DynamoDB } from 'aws-sdk';
-import { APIGatewayProxyResult } from 'aws-lambda';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import * as uuid from 'uuid';
+import dynamoDB from './libs/dynamodb-lib';
+import handler from './libs/handler-lib';
 
-const dynamoDB = new DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
-
   const params: DocumentClient.PutItemInput = {
-    TableName: process.env.tableName, 
+    TableName: process.env.tableName,
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: uuid.v1(),
-      content: data.content, 
-      attachment: data.attachment, 
-      createdAt: Date.now()
-    }
-  }
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now(),
+    },
+  };
 
-  // callback(null,JSON.stringify(process.env));
-  // return;
-  dynamoDB.put(params, (error: AWSError, data: DocumentClient.PutItemOutput) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
+  await dynamoDB.put(params);
 
-    if(error) {
-      const response: APIGatewayProxyResult = {
-        statusCode: 500,
-        headers: headers, 
-        body: JSON.stringify(error)
-      }
-      callback(null, response);
-      return;
-    }
-
-    const response: APIGatewayProxyResult = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
-}
+  return params.Item;
+});
