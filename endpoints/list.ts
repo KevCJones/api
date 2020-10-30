d0c35f8045c7aff85f203ad158f1faa1b72de6df
Kevin Jones <kevin.jones@ppushdr.com>
2020-10-30 13:15:09 +0000

    moved calls to endpoints folder and added a list and update call

---
+++ endpoints/list.ts
@@ -0,0 +1,23 @@
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import dynamoDB from '../libs/dynamodb-lib';
+import handler from '../libs/handler-lib';
+
+export const main = handler(async (event, context) => {
+  const params: DocumentClient.QueryInput = {
+    TableName: process.env.tableName,
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be Identity Pool identity id
+    //   of the authenticated user
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': event.requestContext.identity.cognitoIdentityId,
+    },
+  };
+
+  const result = await dynamoDB.query(params);
+
+  return result.Items;
+});
